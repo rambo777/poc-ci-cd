ee7877ad4ed3faa266d516490674eb104f17d0ef
rambo777 <singhranjit517@gmail.com>
2017-10-31 18:56:35 +0530

    Update procedure.sql

--- poc/procedure.sql
+++ poc/procedure.sql
@@ -1,6 +1,6 @@
 DROP PROCEDURE IF EXISTS testHello;
- CREATE PROCEDURE testHello()
-    BEGIN
-      SELECT 'Hello Worlds!!!!!!!!!!!!!!!!1';
-    END
+CREATE PROCEDURE testHello()
+BEGIN
+SELECT 'Hello Worlds!!!!!!!!!!!!!!!!1';
+END
 
