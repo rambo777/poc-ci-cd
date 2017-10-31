e8b83223e61fe9b82db9bf9ffdd6b416e17b9a5b
rambo777 <singhranjit517@gmail.com>
2017-10-31 19:44:04 +0530

    Update procedure.sql

--- poc/procedure.sql
+++ poc/procedure.sql
@@ -2,6 +2,6 @@ DROP PROCEDURE IF EXISTS sayHelloWorld;
 #
 CREATE PROCEDURE sayHelloWorld()
 BEGIN
-    SELECT 'Hello World From a MySql Database!';
+    SELECT 'Hello World From a Liquibase!';
 END
 #
