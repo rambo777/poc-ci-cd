5f31128bfecc338c7c266112a5aa57479e7843fc
rambo777 <singhranjit517@gmail.com>
2017-11-06 17:26:45 +0530

    Update changelog.sql

--- poc/changelog.sql
+++ poc/changelog.sql
@@ -1,18 +1,5 @@
 --liquibase formatted sql
---changeset nvoxland:1
-create table employees (
-    id int primary key,
-    name varchar(255)
-);
---changeset nvoxland:2 runOnChange:true
-insert into employees (id, name) values (1, 'dev');
-insert into employees (id, name) values (2, 'ops');
 
---changeset nvoxland:3
-create table employeesrishi (
-    id int primary key,
-    name varchar(255)
-);
 --changeset nvoxland:4 runOnChange:true
-insert into employeesrishi (id, name) values (1, 'dev');
-insert into employeesrishi (id, name) values (2, 'ops');
+insert into employeesrishi (id, name) values (1, 'db');
+insert into employeesrishi (id, name) values (2, 'dbrishi');
