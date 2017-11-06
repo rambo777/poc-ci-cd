616b2f7bdb573b92a6b7d45630e042e116547fe3
rambo777 <singhranjit517@gmail.com>
2017-11-06 17:27:39 +0530

    Update changelog.sql

--- poc/changelog.sql
+++ poc/changelog.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 
 --changeset nvoxland:4 runOnChange:true
-insert into employeesrishi (id, name) values (1, 'db');
-insert into employeesrishi (id, name) values (2, 'dbrishi');
+insert into employeesrishi (id, name) values (3, 'db');
+insert into employeesrishi (id, name) values (4, 'dbrishi');
