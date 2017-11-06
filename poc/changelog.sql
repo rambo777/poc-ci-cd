1ff07edbc4c25c50bb3c5692e5c43e78c01859d7
rambo777 <singhranjit517@gmail.com>
2017-11-06 17:24:24 +0530

    Update changelog.sql

--- poc/changelog.sql
+++ poc/changelog.sql
@@ -7,3 +7,12 @@ create table employees (
 --changeset nvoxland:2 runOnChange:true
 insert into employees (id, name) values (1, 'dev');
 insert into employees (id, name) values (2, 'ops');
+
+--changeset nvoxland:3
+create table employeesrishi (
+    id int primary key,
+    name varchar(255)
+);
+--changeset nvoxland:4 runOnChange:true
+insert into employeesrishi (id, name) values (1, 'dev');
+insert into employeesrishi (id, name) values (2, 'ops');
