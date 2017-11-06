5b9a126f5c66beef6b0b2b27b6e744444d8f65db
rambo777 <singhranjit517@gmail.com>
2017-11-06 20:11:10 +0530

    Update changelog.sql

--- poc/changelog.sql
+++ poc/changelog.sql
@@ -11,7 +11,7 @@ END
 --changeset nvoxland:6 runOnChange:true
 create table employeerishi (empId int);
 create table addressdetails (empId int);
-create tables emp (empId int);
+create tabless emp (empId int);
 --rollback drop table employeerishi,addressdetails;
 
 
