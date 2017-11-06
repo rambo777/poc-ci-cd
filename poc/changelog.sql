765c7e903831bdac84e6456c48e002f278e3f222
rambo777 <singhranjit517@gmail.com>
2017-11-06 17:35:46 +0530

    Update changelog.sql

--- poc/changelog.sql
+++ poc/changelog.sql
@@ -3,3 +3,12 @@
 --changeset nvoxland:4 runOnChange:true
 insert into employeesrishi (id, name) values (3, 'db');
 insert into employeesrishi (id, name) values (4, 'dbrishi');
+
+--changeset ranjit:5 endDelimiter:# runOnChange:true
+DROP PROCEDURE IF EXISTS sayHelloWorld;
+#
+CREATE PROCEDURE sayHelloRishi()
+BEGIN
+    SELECT ' Hello :) Liquibase!';
+END
+#
