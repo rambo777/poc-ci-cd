b02e7982446ef1f1430e384c09d87832e4b0894a
rambo777 <singhranjit517@gmail.com>
2017-11-06 19:32:17 +0530

    Update changelog.sql

--- poc/changelog.sql
+++ poc/changelog.sql
@@ -16,5 +16,5 @@ create table emp (empId int);
 
 --changeset nvoxland:4 runOnChange:true
 insert into employeerishi (id, name) values (3, 'db');
-insert into employeerishi (id, name) values (4, 'dbrishi');
+insert into employeerishii (id, name) values (4, 'dbrishi');
 
